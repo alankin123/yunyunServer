9d7be2d9bba758b7adf55d2021af7a3804948c9b
alankin <460682402@qq.com>
2018-12-05 17:04:55 +0800

    11

--- center-user/center-user-service/src/main/java/com/alankin/Setup.java
+++ center-user/center-user-service/src/main/java/com/alankin/Setup.java
@@ -3,18 +3,19 @@ package com.alankin;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Setup {
-	@SuppressWarnings({ "resource" })
-	public static void main(String[] args) throws Exception {			
-		System.setProperty("dubbo.application.name", "center-user-local");
-		System.setProperty("dubbo.consumer.timeout", "80000");
-		System.setProperty("dubbo.consumer.retries", "0");
-		System.setProperty("dubbo.registry.address", "zookeeper://192.168.235.130:2181?register=true");
-		System.setProperty("dubbo.protocol.port", "20101");
-		// 加载spring容器
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:META-INF/spring/applicationContext-provider.xml");
-		context.start();
-
-		System.out.println("Press any key to exit.");
-		System.in.read();
-	}
+    @SuppressWarnings({"resource"})
+    public static void main(String[] args) throws Exception {
+        System.out.println("设置dubbo连接参数.");
+        System.setProperty("dubbo.application.name", "center-user-local");
+        System.setProperty("dubbo.consumer.timeout", "80000");
+        System.setProperty("dubbo.consumer.retries", "0");
+        System.setProperty("dubbo.registry.address", "zookeeper://192.168.140.128:2181?register=true");
+        System.setProperty("dubbo.protocol.port", "20101");
+        System.out.println("初始化spring容器");
+        // 加载spring容器
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:META-INF/spring/applicationContext-provider.xml");
+        context.start();
+        System.out.println("启动成功");
+        System.in.read();
+    }
 }
